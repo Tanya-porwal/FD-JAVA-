932791509ee0be6a1a4eb36f540d0863006de1ae
Tanya-porwal <tanyaporwal9027@gmail.com>
2025-08-12 09:32:11 +0530

    Update userinput.java

--- userinput.java
+++ userinput.java
@@ -1,17 +1,19 @@
-
-
 import java.util.*;
 public class userinput {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a no");
+        int a = sc.nextInt();
+        System.out.println("Enter an other no ");
+        int b = sc.nextInt();
+        System.out.println("The sum of two no." + a + " and " + b + " is" + a+b );
+
+        System.out.println("Enter a string");
         String name = sc.next();
-        int no = sc.nextInt();
-        float price =sc.nextFloat();
-        String names = sc.nextLine();
-        
         System.out.println(name);
-        System.out.println(names);
-        System.out.println(no);
+
+        System.out.println("Enter a Float");
+        float price =sc.nextFloat();
         System.out.println(price);
 
         sc.close();
@@ -19,3 +21,4 @@ public class userinput {
     }
     
 }
+
