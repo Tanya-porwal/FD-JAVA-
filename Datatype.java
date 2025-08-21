6f91b91dcbc1bffb17e19221053d05127dc65e82
Tanya-porwal <tanyaporwal9027@gmail.com>
2025-08-21 13:18:06 +0530

    Update Datatype.java

---
+++ Datatype.java
@@ -0,0 +1,19 @@
+public class Datatype {
+    public static void main(String[] args) {
+        String name = "Tanya";
+        int [] a ={1,4,6,9,0,8,6};
+        int[] b ={1,4,6,7,8};
+        System.out.println(name);
+            System.out.println(a[0]);
+            System.out.println(a[1]);
+            System.out.println(a[2]);
+            System.out.println(a[3]);
+            System.out.println(a[4]);
+            System.out.println(a[5]);
+            System.out.println(b[0]);
+            System.out.println(b[1]);
+            System.out.println(b[2]);
+    }
+}
+        
+       
