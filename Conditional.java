d923f29edb09990b5884d4a43d246ed0aef0ada9
Tanya-porwal <tanyaporwal9027@gmail.com>
2025-08-25 11:01:37 +0530

    Add files via upload

---
+++ Conditional.java
@@ -0,0 +1,16 @@
+import java.util.* ;
+public class Conditional {
+    public void check(){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter a .");
+        int n = sc.nextInt();
+        if(n==1){
+            System.out.println("");
+        }
+    }
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
