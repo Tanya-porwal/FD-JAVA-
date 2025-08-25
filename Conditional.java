286b87c9ce57aba1aeb0ec20abc34350a385be71
Tanya-porwal <tanyaporwal9027@gmail.com>
2025-08-25 11:02:39 +0530

    Update Conditional.java

--- Conditional.java
+++ Conditional.java
@@ -1,16 +1,26 @@
-import java.util.* ;
+import java.util.Scanner;
+
 public class Conditional {
-    public void check(){
-        Scanner sc= new Scanner(System.in);
-        System.out.println("Enter a .");
-        int n = sc.nextInt();
-        if(n==1){
-            System.out.println("");
+    public void check() {
+        Scanner sc = new Scanner(System.in); // Scanner object
+        System.out.print("Enter your marks: ");
+        int marks = sc.nextInt(); // user input
+
+        if (marks == 10) {
+            System.out.println("O");
+        } else if (marks == 9) {
+            System.out.println("A+");
+        } else if (marks == 8) {
+            System.out.println("A");
+        } else {
+            System.out.println("Other grade");
         }
+
+        sc.close(); 
     }
+
     public static void main(String[] args) {
-        
-        
+        Conditiona c = new Conditiona();
+        c.check();
     }
-    
 }
