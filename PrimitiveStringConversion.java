422995a2679b67d524d97e6446e881df277197e2
Tanya-porwal <tanyaporwal9027@gmail.com>
2025-08-26 09:09:26 +0530

    Add files via upload

---
+++ PrimitiveStringConversion.java
@@ -0,0 +1,12 @@
+public class PrimitiveStringConversion {
+    public static void main(String[] args) {
+        int num =100 ;
+        String str1 = String.valueOf(num);
+        String str2 = Integer.toString(num);
+        String s = "100";
+        int parsedInt = Integer.parseInt(s);
+        System.out.println("String from int:" + str1);
+        System.out.println("Parsed int from String: " + parsedInt);
+        System.out.println("😁😁😁😁😂😂😂");
+    }
+}
