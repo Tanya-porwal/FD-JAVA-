55b6b219e267332a88c00e8159d135d0ac959bbf
Tanya-porwal <tanyaporwal9027@gmail.com>
2025-08-26 09:46:21 +0530

    Add files via upload

---
+++ ArraysExample.java
@@ -0,0 +1,13 @@
+import java.util.* ;
+public class ArraysExample {
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 1, 4, 2};
+        System.out.println("Original: "+ Arrays.toString(arr));
+        Arrays.sort(arr);
+        System.out.println("Sorted: " + Arrays.toString(arr));
+        int[] copy = Arrays.copyOf(arr, 7);
+        System.out.println("Copy: " + Arrays.toString(copy));
+        Arrays.fill(copy, 6);
+        System.out.println("Filled: "+Arrays.toString(copy));
+    }
+}
