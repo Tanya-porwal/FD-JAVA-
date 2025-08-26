055110a4e834dbcf9dd7faded38792ce85ec901a
Tanya-porwal <tanyaporwal9027@gmail.com>
2025-08-26 09:00:31 +0530

    Add files via upload

---
+++ ImplicitConversion.java
@@ -0,0 +1,12 @@
+public class ImplicitConversion {
+    public static void main(String[] args) {
+        int num = 100 ;
+        long irrrgrn = 121633;
+        System.out.println(num);
+        System.out.println(irrrgrn);
+        double d=100.99 ;
+        int n=(int)d ;
+        System.out.println(d);
+        System.out.println(n);
+    }
+}
