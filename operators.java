5b8643e6020b5eeb246131ba0f435e930c5cedf1
Tanya-porwal <tanyaporwal9027@gmail.com>
2025-09-01 21:55:01 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,43 @@
+public class operators {
+
+        // Your code here
+        // arithmetic operators(+,-,*,/,%,++,--)
+        // relational operators (==,<=,>=;!=)
+        // logical operators(&& ,|| ,!)
+        // assignment operators(=,+=,-=,/=,%=)
+        // unary operators(=,-,++,--,!)
+        // bitwise operators(|,&,^,~,<<,>>, )
+    public static void main(String[] args) {
+        // arithmetic operators 
+        int a = 4;
+        int b = 8;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(b%a);
+        // relational operators 
+        System.out.println(a==b);
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+        System.out.println(a!=b);
+        // assignment operators 
+        System.out.println(a==5);
+        System.out.println(a+=1);
+        System.out.println(a-=1);
+        System.out.println(a/=1);
+        System.out.println(a*=2);
+        System.out.println(a%=2);
+        // unary operators 
+        System.out.println();
+        
+        // bitwise operators 
+        System.out.println(a&b);
+        System.out.println(a|b);
+        System.out.println(a^b);
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+    }
+}
