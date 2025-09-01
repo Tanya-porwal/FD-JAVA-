efa10f335e59ca471eb6d012cbc70f9ea2417d25
Tanya-porwal <tanyaporwal9027@gmail.com>
2025-09-01 21:51:30 +0530

    Create Bitlevel.java

---
+++ Bitlevel.java
@@ -0,0 +1,15 @@
+public class Bitlevel {
+    public static void main(String[] args) {
+        // BITWISE OPERATORS
+        int a = 5;  
+        int b = 3;
+        System.out.println(a & b);  
+        System.out.println(a | b);
+        System.out.println(a ^ b);
+        System.out.println(~a);
+        System.out.println(a << 1);
+        System.out.println(a >> 1);
+
+    }
+    
+}
