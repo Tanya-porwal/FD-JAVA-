9e0c1d7b0b22233a5a8fc02b91a21d1bf0cead98
Tanya-porwal <tanyaporwal9027@gmail.com>
2025-09-01 21:56:39 +0530

    Create Datatypen.java

---
+++ Datatypen.java
@@ -0,0 +1,8 @@
+public class Datatypen {
+    public static void main(String[] args) {
+        String name = "Max Mustermann";
+        int arr[] = {1, 2, 3, 4, 5};
+        System.out.println(name);
+        System.out.println(arr[4]);
+    }
+}
